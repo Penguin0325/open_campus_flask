46856f73dc991c9ebbb90b332a95780e63c3dc50
penguin0325 <fierce0728birds@gmail.com>
2024-05-30 19:01:57 +0900

    cssテスト

--- testapp/views.py
+++ testapp/views.py
@@ -12,3 +12,39 @@ def index():
 @app.route('/test')
 def test():
     return render_template('test.html')
+
+@app.route('/story1')
+def test():
+    return render_template('story1.html')
+
+@app.route('/story2')
+def test():
+    return render_template('story2.html')
+
+@app.route('/story3')
+def test():
+    return render_template('story3.html')
+
+@app.route('/story4')
+def test():
+    return render_template('story4.html')
+
+@app.route('/story5')
+def test():
+    return render_template('story5.html')
+
+@app.route('/story6')
+def test():
+    return render_template('story6.html')
+
+@app.route('/story7')
+def test():
+    return render_template('story7.html')
+
+@app.route('/story8')
+def test():
+    return render_template('story8.html')
+
+@app.route('/story9')
+def test():
+    return render_template('story9.html')
