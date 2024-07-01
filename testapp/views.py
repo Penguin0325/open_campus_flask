a7fd5f3e1ec0675b01880e5ae46f90e59eadb584
penguin0325 <fierce0728birds@gmail.com>
2024-07-01 15:25:58 +0900

    gitにあげた

--- testapp/views.py
+++ testapp/views.py
@@ -27,37 +27,41 @@ def test():
 def hints():
     return render_template('hints.html')
 
+@app.route('/character')
+def character():
+    return render_template('character.html')
+
 @app.route('/story1')
 def story1():
     return render_template('story1.html')
 
 @app.route('/anser1')
 def anser1():
-    return render_template('mystery1anser')
+    return render_template('mystery1anser.html')
 
 @app.route('/anser2')
 def anser2():
-    return render_template('mystery2anser')
+    return render_template('mystery2anser.html')
 
 @app.route('/anser3')
 def anser3():
-    return render_template('mystery3anser')
+    return render_template('mystery3anser.html')
 
-@app.route('/story5')
-def story5():
-    return render_template('story5.html')
+@app.route('/middlestory')
+def middlestory():
+    return render_template('middlestory.html')
 
-@app.route('/story6')
-def story6():
-    return render_template('story6.html')
+@app.route('/afterentering')
+def afterentering():
+    return render_template('afterentering.html')
 
-@app.route('/story7')
-def story7():
-    return render_template('story7.html')
+@app.route('/inputchatbot1')
+def inputchatbot1():
+    return render_template('inputchatbot1.html')
 
-@app.route('/story8')
-def story8():
-    return render_template('story8.html')
+@app.route('/inputchatbot2')
+def inputchatbot2():
+    return render_template('inputchatbot2.html')
 
 @app.route('/story9')
 def story9():
